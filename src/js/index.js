ae60939cb8b31b0b6b739131ea01b5b85bb9aaed
Luan Ferreira <81898114+lusoferreiras@users.noreply.github.com>
2023-01-22 23:52:00 -0300

    projeto finalizado

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+/*
+OBJETIBO 1 - quando o usuário clicar no botão de ver trailer, devemos abrir a modal com o vídeo do trailer
+- passo 1 - dar um jeito de pegar o elemento que representa o botão na tela usando o js
+
+- passo 2 - dar um jeito de identificar quando o usuário clicar no botão
+
+- passo 3 - dar um jeito de pegar o elemento da modal no js
+
+- passo 4 - abrir a modal na tela
+
+OBJETIVO 2 - quando o usuário clicar no X devemos fechar a modal
+
+- passo 1 - dar um jeito de pegar o elemento de fechar modal usando o js
+
+- passo2 - dar um jeito de identificar quando o usuário clicar no X
+
+- passo 3 - fechar a modal
+
+*/
+
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+
+
